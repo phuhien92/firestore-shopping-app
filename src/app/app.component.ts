7b20dc163cc95026fd78c11c1c5638db651ba28c
hluong <hluong@AdminUsers-MacBook-Pro.local>
2017-12-05 10:53:20 -0800

    update auth.js

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,7 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { TabsPage } from '../pages/tabs/tabs';
+import { AngularFireAuth } from 'angularfire2/auth';
 
 @Component({
   templateUrl: 'app.html'
@@ -11,7 +12,19 @@ import { TabsPage } from '../pages/tabs/tabs';
 export class MyApp {
   rootPage:any = TabsPage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(
+    platform: Platform, 
+    statusBar: StatusBar, 
+    splashScreen: SplashScreen,
+    afAuth: AngularFireAuth
+  ) {
+    afAuth.authState.subscribe( user => {
+      if (user) {
+        this.rootPage = TabsPage;
+      } else {
+        this.rootPage = 'LoginPage';
+      }
+    })
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
--- src/providers/auth/auth.ts
+++ src/providers/auth/auth.ts
@@ -1,17 +1,73 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { AngularFireAuth } from 'angularfire2/auth';
+import {
+  AngularFirestore,
+  AngularFirestoreCollection,
+  AngularFirestoreDocument
+} from 'angularfire2/firestore';
+import firebase from 'firebase/app';
+import { userProfile } from '../../models/user-profile';
+import { teamProfile } from '../../models/team-profile';
 
-/*
-  Generated class for the AuthProvider provider.
-
-  See https://angular.io/guide/dependency-injection for more info on providers
-  and Angular DI.
-*/
 @Injectable()
 export class AuthProvider {
 
-  constructor(public http: HttpClient) {
-    console.log('Hello AuthProvider Provider');
+  constructor(
+    public afAuth: AngularFireAuth,
+    public fireStore: AngularFirestore
+  ) {
+    
   }
 
+  loginUser(email: string): Promise<void> {
+    return this.afAuth.auth.sendPasswordResetEmail(email);
+  }
+
+  logoutUser(): Promise<void> {
+    return this.afAuth.auth.signOut();
+  }
+  
+  async createAdminUser(email: string, password: string): Promise<firebase.User> {
+    try {  
+      const adminUser: firebase.User = await this.afAuth.auth.
+      createUserWithEmailAndPassword(email, password);
+
+      const userProfileDocument: AngularFirestoreDocument<userProfile> = this.fireStore.doc(`userProfile/${adminUser.uid}`);
+
+      await userProfileDocument.set({
+        id: adminUser.uid,
+        email: adminUser.email,
+        teamAdmin: true,
+        teamId: adminUser.uid
+      });
+
+      const teamProfile: AngularFirestoreDocument<teamProfile> = this.fireStore.doc(`teamProfile/${adminUser.uid}`);
+
+      await teamProfile.set({
+        id: adminUser.uid,
+        teamAdmin: adminUser.uid,
+        groceryList: null
+      });
+
+        return adminUser;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  createRegularUser(email: string): Promise<any> {
+    const teamAdmin: firebase.User = this.afAuth.auth.currentUser;
+    const userCollection: AngularFirestoreCollection<any> = this.fireStore.collection(`teamProfile/${teamAdmin.uid}/teamMemberList`);
+
+    const id: string = this.fireStore.createId();
+
+    const regularUser = {
+      id: id,
+      email: email,
+      teamId: teamAdmin.uid
+    };
+
+    return userCollection.add(regularUser);
+  }
 }
